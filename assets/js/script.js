a82b8243d903ac7b906f9723e4b3195a3b635a96
Siena Schipke <sienaschipke@gmail.com>
2024-06-11 18:25:35 -0400

    updating todays weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,7 @@ const submitBtnHandler = event => {
     }
 };
 
+// This function takes the city name as an input and converts the city to 
 const getLatAndLong = city => {
     const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},US&limit=1&appid=ee7c96e3014aea6e9033db179b95b0cc`;
     
@@ -30,7 +31,8 @@ const getLatAndLong = city => {
         .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-            getWeatherData(data[0].lat, data[0].lon);
+            getForecastData(data[0].lat, data[0].lon);
+            getTodaysWeather(data[0].lat, data[0].lon);
             });
         } else {
             alert(`Error: ${response.statusText}`);
@@ -41,15 +43,36 @@ const getLatAndLong = city => {
         });
     };
 
-const getWeatherData = (lat, lon) => {
+const getTodaysWeather = (lat, lon) => {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=ee7c96e3014aea6e9033db179b95b0cc`;
+    
+    fetch(apiUrl)
+        .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            // displayForecast(data);
+            // displayToday(data);
+            });
+        } else {
+            alert(`Error:${response.statusText}`);
+        }
+        })
+        .catch(function (error) {
+        alert('Unable to connect to weather data');
+        });
+    };
+    
+
+const getForecastData = (lat, lon) => {
     const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=ee7c96e3014aea6e9033db179b95b0cc`;
     
     fetch(apiUrl)
         .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-            displayWeather(data);
-            // console.log(data);
+                displayForcast(data);
+            // displayToday(data);
             });
         } else {
             alert(`Error:${response.statusText}`);
@@ -60,7 +83,7 @@ const getWeatherData = (lat, lon) => {
         });
     };
 
-const displayWeather = (data) => {
+const displayForcast = (data) => {
     console.log(data);
     const todaysDate = dayjs.unix(data.list[0].dt);
     let futureDate;
